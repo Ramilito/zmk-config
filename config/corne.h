c0dd82a2d1e7b6fd4cc6ae6c5b002f96e59b9f27
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-12-13 21:58:52 +0100

    added stuff

--- config/corne.h
+++ config/corne.h
@@ -8,8 +8,13 @@
 #define SK(KEY) &nk KEY KEY
 #define MO(LAYER) &mo_tap LAYER F24
 
+#define U_MT(MOD, TAP) &u_mt MOD TAP
+#define U_LT(LAYER, TAP) &u_lt LAYER TAP
+
 // Keep sticky keys active for a long time so they effectively do not time out
 #define STICKY_KEY_TIMEOUT 60000
+
+#define U_TAPPING_TERM 200
 
 // Apple "Globe" key
 // https://github.com/zmkfirmware/zmk/issues/947
