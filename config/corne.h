48ecbef36313121850e44e32c7e84c915ad76a37
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-12-13 23:56:28 +0100

    trying with tap dance

--- config/corne.h
+++ config/corne.h
@@ -10,7 +10,7 @@
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
-
+#define U_TD(ONE, TWO) &u_td ONE TWO
 // Keep sticky keys active for a long time so they effectively do not time out
 #define STICKY_KEY_TIMEOUT 60000
 
