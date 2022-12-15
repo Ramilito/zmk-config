fc0024636a75230948ccc17c0fe2768a7abe65e8
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-12-15 09:55:52 +0100

    longer combo timeout

--- config/corne.h
+++ config/corne.h
@@ -23,6 +23,7 @@
 // Keep sticky keys active for a long time so they effectively do not time out
 #define STICKY_KEY_TIMEOUT 60000
 
+#define COMBO_TIMEOUT 100
 #define U_TAPPING_TERM 200
 
 // Apple "Globe" key
